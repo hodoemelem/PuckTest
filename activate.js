1b9d05d2a88b0b88aef4e8ef4f445f90952bd691
Henry Ugochukwu Odoemelem <hodoemelem@gmail.com>
2020-11-26 14:13:43 +0100

    Create activate.js

---
+++ activate.js
@@ -0,0 +1,10 @@
+navigator.bluetooth.requestDevice({ filters: [{ services: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e'] }] })
+.then(device => {
+  // Human-readable name of the device.
+  console.log(device.name);
+
+  // Attempts to connect to remote GATT Server.
+  return device.gatt.connect();
+})
+.then(server => { /* … */ })
+.catch(error => { console.error(error); });
